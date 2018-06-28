a26c81b45999556d0824939162e65d5dda5d4275
YSRKEN <ysr.ken@gmail.com>
2018-06-28 21:15:44 +0900

    config css and initial main.component(テキトーに初期化)

--- CalcGachaProbTool/src/app/app.module.ts
+++ CalcGachaProbTool/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { MainComponent } from './main/main.component';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-    RouterModule
+    RouterModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
