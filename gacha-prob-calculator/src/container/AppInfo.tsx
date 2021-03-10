1dc575bc4f0cabc98b585ad7a22f8009e3de1b1a
YSRKEN <ysr.ken@gmail.com>
2021-03-10 11:28:00 +0900

    [add] レスポンシブ対応を進めた、また、アイコンのたぐいを移植した

--- gacha-prob-calculator/src/container/AppInfo.tsx
+++ gacha-prob-calculator/src/container/AppInfo.tsx
@@ -2,8 +2,8 @@ import { Button } from "react-bootstrap";
 import { LAST_UP_DATE, PROJECT_URL, TWITTER_SHARE_URL } from "../constant/other";
 
 const AppInfo: React.FC = () => (<>
-  <span className="d-inline-block mr-3">最終更新：{LAST_UP_DATE}</span>
-  <span className="d-inline-block mr-3"><a href={PROJECT_URL} rel="noreferrer" target="_blank">プロジェクトURL</a></span>
+  <span className="mr-3 d-none d-sm-inline">最終更新：{LAST_UP_DATE}</span>
+  <span className="mr-3"><a href={PROJECT_URL} rel="noreferrer" target="_blank">プロジェクトURL</a></span>
   <Button size="sm" href={TWITTER_SHARE_URL} target="_blank">Twitterにシェア</Button>
 </>);
 
