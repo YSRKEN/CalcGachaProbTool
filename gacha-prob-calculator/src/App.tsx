d3db0f07fbfc061967366833e9525cd48aa6d6fc
YSRKEN <ysr.ken@gmail.com>
2021-03-10 11:37:54 +0900

    [add] 取り急ぎタブを生やした

--- gacha-prob-calculator/src/App.tsx
+++ gacha-prob-calculator/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
 import Title from './container/Title';
 import AppInfo from './container/AppInfo';
 
@@ -15,6 +15,21 @@ const App: React.FC = () => {
       <Row>
         <Col className="text-center">
           <AppInfo />
+        </Col>
+      </Row>
+      <Row className="mt-3 justify-content-md-center">
+        <Col md={6}>
+          <Tabs defaultActiveKey="calc" id="main-tab">
+            <Tab className="border-bottom border-left border-right p-3" eventKey="calc" title="確率計算">
+              aaa
+            </Tab>
+            <Tab className="border-bottom border-left border-right p-3" eventKey="test" title="確率推定">
+              bbb
+            </Tab>
+            <Tab className="border-bottom border-left border-right p-3" eventKey="help" title="ヘルプ">
+              ccc
+            </Tab>
+          </Tabs>
         </Col>
       </Row>
     </Container>
