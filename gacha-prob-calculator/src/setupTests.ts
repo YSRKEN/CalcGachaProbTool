7bc68ffd9401350a8a3332997551aa1a5910675c
YSRKEN <ysr.ken@gmail.com>
2026-03-26 12:34:42 +0900

    build(react): migrate CRA to Vite + React 18 + Vitest

--- gacha-prob-calculator/src/setupTests.ts
+++ gacha-prob-calculator/src/setupTests.ts
@@ -1,14 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
 
-if (typeof global.clearImmediate === 'undefined') {
-  global.clearImmediate = ((id: NodeJS.Timeout) => clearTimeout(id)) as typeof clearImmediate;
+if (typeof globalThis.clearImmediate === 'undefined') {
+  globalThis.clearImmediate = ((id: ReturnType<typeof setTimeout>) =>
+    clearTimeout(id as number)) as typeof clearImmediate;
 }
 
-if (typeof global.setImmediate === 'undefined') {
-  global.setImmediate = ((callback: (...args: any[]) => void, ...args: any[]) =>
+if (typeof globalThis.setImmediate === 'undefined') {
+  globalThis.setImmediate = ((callback: (...args: unknown[]) => void, ...args: unknown[]) =>
     setTimeout(callback, 0, ...args)) as typeof setImmediate;
 }
